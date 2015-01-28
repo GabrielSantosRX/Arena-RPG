cfc7f90430d074c5029b678ff47f2ebc2703cbd0
Gabriel Santos <gabrielaraujo182@gmail.com>
2015-01-28 15:14:20 -0300

    Create Battleground.py

---
+++ src/Battleground.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+class Battleground:
+    """ Arena, campo aberto para a pratica da porradaria """
+    
+    def __init__(self, name, width, height):
+        self.name = name
+        self.width = width
+        self.height = height
